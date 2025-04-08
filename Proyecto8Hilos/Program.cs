44fe885eb59b36b3d7ed86e70445751487cbdf09
nallely-lopez <lopezgarcializethnallely@gmail.com>
2025-04-07 21:08:31 -0600

    Agregar proyecto al repositorio

---
+++ Proyecto8Hilos/Program.cs
@@ -0,0 +1,6 @@
+﻿using System; //importación de librerías, permnite usar funciones básicas (writeLine)
+using System.Threading;//importa la funcionalidad necesaria para utilizar hilos 
+class Program
+{
+    
+}

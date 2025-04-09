4bafe69a6c6c1898ed4b889a18637757d5813a47
Ab396 <abelardoortizortiz31@gmail.com>
2025-04-09 12:32:41 -0600

    se implemento la libreria de datatime y  mensajes del reloj alarma

--- Proyecto8Hilos/Program.cs
+++ Proyecto8Hilos/Program.cs
@@ -2,5 +2,16 @@
 using System.Threading;//importa la funcionalidad necesaria para utilizar hilos 
 class Program
 {
-    
+
+    static DateTime horaAlarma;
+    static bool alarmaActivada = false;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-------- Reloj con Alarma ----------");
+        Console.Write("Ingresa la hora de la alarma (formato HH:mm:ss Tomando en cuenta el formato de 24 hrs): ");
+
+        string entrada = Console.ReadLine();
+
+    }
 }
--- Proyecto8Hilos/obj/Release/net8.0/Proyecto8Hilos.AssemblyInfo.cs
+++ Proyecto8Hilos/obj/Release/net8.0/Proyecto8Hilos.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto8Hilos")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99dcb537d06239651a887b261617ee04071d927c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto8Hilos")]
